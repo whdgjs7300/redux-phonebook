080313f6faa50fc844d596f857658a342473d011
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-04-04 14:57:47 +0900

    4/4 bootstrap

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import ContactForm from './components/ContactForm';
+import {Container, Row, Col} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// 1. 왼쪽에는 연락처 리스트와 , 오른쪽에는 연락처 리스트와 search 창이 있다
+// 2. 유저가 이름과 폰넘버를 입력하면 전화번호를 추가할 수 있다.
+// 3. 리스트의 아이템이 몇개 인지 보인다.
+// 4. 사용자가 유저를 이름검색으로 찾을 수 있다.
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='title'>연락처</h1>
+      <Container>
+        <Row>
+          <Col>
+              <ContactForm/>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
