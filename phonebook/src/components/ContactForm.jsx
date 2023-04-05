6f56cbaff48263cf052d8b57feafca8eacb5a65d
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-04-05 17:07:03 +0900

    4/5 state

--- phonebook/src/components/ContactForm.jsx
+++ phonebook/src/components/ContactForm.jsx
@@ -1,19 +1,25 @@
+import { useState } from 'react';
 import {Form, Button} from 'react-bootstrap';
 
 
 const ContactForm = () => {
+    const [name, setName] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState(0);
+
+
+    
 
     return ( 
         <div>
             <Form>
         <Form.Group className="mb-3" controlId="formName">
             <Form.Label>이름</Form.Label>
-            <Form.Control type="text" placeholder="이름을 입력하세요" />
+            <Form.Control type="text" placeholder="이름을 입력하세요" onChange={(e)=>setName(e.target.value)} />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formContact">
             <Form.Label>전화번호</Form.Label>
-            <Form.Control type="number" placeholder="전화번호를 입력하세요" />
+            <Form.Control type="number" placeholder="전화번호를 입력하세요" onChange={(e)=>setPhoneNumber(e.target.value)} />
         </Form.Group>
         
         <Button variant="primary" type="submit">
